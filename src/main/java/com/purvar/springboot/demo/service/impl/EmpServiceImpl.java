c36407c9c03df22dc18f2fefe0e1731f2ceb1846
panqi <panqi@purvar.com>
2021-01-16 10:18:22 +0800

    upload

---
+++ src/main/java/com/purvar/springboot/demo/service/impl/EmpServiceImpl.java
@@ -0,0 +1,28 @@
+package com.purvar.springboot.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.purvar.springboot.demo.bean.Emp;
+import com.purvar.springboot.demo.mapper.EmpMapper;
+import com.purvar.springboot.demo.service.EmpService;
+
+@Service
+public class EmpServiceImpl implements EmpService {
+	@Autowired
+	private EmpMapper mapper;
+	
+	@Override
+	@Transactional(rollbackFor = Exception.class)
+	public List<Emp> getEmpList(String empId) {
+		return mapper.getEmpListById(empId);
+	}
+	
+	@Override
+	public List<Emp> getEmpList() {
+		return mapper.getEmpList();
+	}
+}
